ac4984886eed9bb3d24b92556ef0ca4a4cd19cc4
bqio <dmitry.mikhaltsov98@gmail.com>
2024-09-12 17:27:42 +0300

    added private methods

--- binarypacker/core.py
+++ binarypacker/core.py
@@ -4,53 +4,53 @@ from pathlib import Path
 
 import sys, inspect
 
-def get_types() -> tuple:
-    _types = inspect.getmembers(sys.modules["binarypacker.types"], inspect.isclass)
-    out = ()
-    for _type in _types:
-        out += (_type[1],)
-    return out
-
-def parse_format(obj: object) -> str:
-    out = ""
-    for field_name in obj.__class__.__annotations__:
-        field = obj.__class__.__annotations__[field_name]
-        if isinstance(field, get_types()):
-            if type(field) == type(UTF8String):
-                _str = obj.__dict__[field_name]
-                out += str(len(_str.encode('utf-8'))) + str(field)
-            else:
-                out += str(field)
-        else:
-            out += parse_format(obj.__getattribute__(field_name))
-    return out
-
-def pack(obj: object) -> tuple:
-    buf = ()
-    for key in obj.__dict__:
-        field = obj.__dict__[key]
-        if issubclass(type(field), BinaryModel):
-            buf += pack(field)
-        else:
-            if isinstance(field, str):
-                buf += (field.encode('utf-8'),)
-            else:
-                buf += (field,)
-    return buf
-
-def prepare_pack(obj: object, fmt: str) -> bytes:
-    buf = pack(obj)
-    return pk(fmt, *buf)
-
 class BinaryModel:
+    def __get_types() -> tuple:
+        types = inspect.getmembers(sys.modules["binarypacker.types"], inspect.isclass)
+        out = ()
+        for type in types:
+            out += (type[1],)
+        return out
+
+    def __parse_format(self, obj: object) -> str:
+        out = ""
+        for field_name in obj.__class__.__annotations__:
+            field = obj.__class__.__annotations__[field_name]
+            if isinstance(field, self.__get_types()):
+                if type(field) == type(UTF8String):
+                    _str = obj.__dict__[field_name]
+                    out += str(len(_str.encode('utf-8'))) + str(field)
+                else:
+                    out += str(field)
+            else:
+                out += self.__parse_format(obj.__getattribute__(field_name))
+        return out
+
+    def __prepare_pack(self, obj: object, fmt: str) -> bytes:
+        buf = self.__pack(obj)
+        return pk(fmt, *buf)
+    
+    def __pack(self, obj: object) -> tuple:
+        buf = ()
+        for key in obj.__dict__:
+            field = obj.__dict__[key]
+            if issubclass(type(field), BinaryModel):
+                buf += self.__pack(field)
+            else:
+                if isinstance(field, str):
+                    buf += (field.encode('utf-8'),)
+                else:
+                    buf += (field,)
+        return buf
+    
     def get_format(self) -> str:
-        return parse_format(self)
+        return self.__parse_format(self)
     
     def packBig(self) -> bytes:
-        return prepare_pack(self, ">" + self.get_format())
+        return self.__prepare_pack(self, ">" + self.get_format())
     
     def packLittle(self) -> bytes:
-        return prepare_pack(self, "<" + self.get_format())
+        return self.__prepare_pack(self, "<" + self.get_format())
     
     def dumpLittle(self, out_path: Path) -> None:
         with open(out_path, "wb") as fp:
