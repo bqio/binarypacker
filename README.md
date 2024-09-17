df8591201d8b9bed832a834d4f63e1d99874dfe3
bqio <dmitry.mikhaltsov98@gmail.com>
2024-09-17 17:33:01 +0300

    implementation String/NTString

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ pip install git+https://github.com/bqio/binarypacker.git
 #### Usage
 ```python
 from dataclasses import dataclass
-from binarypacker.core import SerializableObject
+from binarypacker.core import SerializableObject, expand
 from binarypacker.endian import BigEndian
 from binarypacker.types import *
 
@@ -29,29 +29,36 @@ class Header(SerializableObject):
     table: Table
 
 @dataclass
+@expand(string_length_t = UInt16)
 class File(SerializableObject):
+    # [string][nt] Default encoding utf-8. Use @expand on class for change string encoding.
+    filename: NTString
+    # [string_length][string] Max string length 255 bytes (uint8) in default. Use @expand on class for change length type.
+    filename2: String
     header: Header
     age: Int8
     size: Int8
 
 table = Table(ptr=120)
 header = Header(offset=24, table=table)
-file = File(header=header, age=2, size=8)
+file = File(filename="data.info", filename2="data2.dat", header=header, age=2, size=8)
 
+# default LittleEndian
 serialized_data = file.serialize(endian=BigEndian)
 
 print(serialized_data)
 
-file2 = File.deserialize(serialized_data)
+file2 = File.deserialize(serialized_data, endian=BigEndian)
 
-print(file2.age, file2.size, type(file2.header.table.ptr))
+print(file.filename, file.filename2, file2.age, file2.size, type(file2.header.table.ptr))
 ```
 #### Output
 ```
-b'\x00\x18x\x02\x08'
-2 8 <class 'binarypacker.types.Int8'>
+b'data.info\x00\x00\tdata2.dat\x00\x18x\x02\x08'
+data.info data2.dat 2 8 <class 'binarypacker.types.Int8'>
 ```
 
 ### TODO
 
-* Implement String and NTString serialization/deserialization
+* ~~Implement String/NTString serialization/deserialization.~~
+* Implement `List` types.
--- binarypacker/core.py
+++ binarypacker/core.py
@@ -1,11 +1,28 @@
 from dataclasses import dataclass, fields
-from struct import pack, unpack, calcsize
+from struct import pack, unpack, calcsize, error as struct_error
 from typing import TypeVar, Type
 
 from .types import *
 from .endian import *
 
 T = TypeVar('T', bound='SerializableObject')
+
+def expand(cls=None, string_length_t = UInt8, string_encoding = "utf-8"):
+    def wrap(cls):
+        setattr(cls, "string_length_t", string_length_t)
+        setattr(cls, "string_encoding", string_encoding)
+        return cls
+    if cls is None:
+        return wrap
+    return wrap(cls)
+
+def get_nt_string_length(buf: bytes):
+    len = 0
+    for b in buf:
+        if b == 0:
+            break
+        len += 1
+    return len
 
 @dataclass
 class SerializableObject(object):
@@ -18,6 +35,9 @@ class SerializableObject(object):
             if issubclass(field_type, SerializableObject):
                 field_value.__post_init__()
             elif issubclass(field_type, num_types):
+                setattr(self, field_name, field_type(field_value))
+            elif issubclass(field_type, str_types):
+                # TODO
                 setattr(self, field_name, field_type(field_value))
             else:
                 raise TypeError(f"Unknown type `{field_type}` on field `{field_name}`. Use only library types.\nMaybe you forgot @dataclass decorator or inheritance SerializableObject on `{field_type}`.")
@@ -32,8 +52,20 @@ class SerializableObject(object):
             if issubclass(field_type, SerializableObject):
                 serialized_data.extend(field_value.serialize(endian))
             else:
-                field_fmt = f"{endian()}{field_type.fmt()}"
-                serialized_data.extend(pack(field_fmt, field_value))
+                if issubclass(field_type, String):
+                    encoding = self.string_encoding if hasattr(self, "string_encoding") else "utf-8"
+                    encoded_str = field_value.encode(encoding)
+                    encoded_str_len = len(encoded_str)
+                    fmt = self.string_length_t.fmt() if hasattr(self, "string_length_t") else "B"
+                    serialized_data.extend(pack(f"{endian()}{fmt}", encoded_str_len))
+                    serialized_data.extend(encoded_str)
+                elif issubclass(field_type, NTString):
+                    encoding = self.string_encoding if hasattr(self, "string_encoding") else "utf-8"
+                    encoded_str = field_value.encode(encoding) + b'\x00'
+                    serialized_data.extend(encoded_str)
+                else:
+                    field_fmt = f"{endian()}{field_type.fmt()}"
+                    serialized_data.extend(pack(field_fmt, field_value))
         return bytes(serialized_data)
     
     @classmethod
@@ -58,6 +90,26 @@ class SerializableObject(object):
                 field_value = field_type.deserialize(serialized_data[offset:offset+size], endian)
                 offset += size
                 setattr(instance, field_name, field_value)
+            elif issubclass(field_type, String):
+                try:
+                    encoding = cls.string_encoding if hasattr(cls, "string_encoding") else "utf-8"
+                    size_fmt = cls.string_length_t.fmt() if hasattr(cls, "string_length_t") else "B"
+                    ssize = calcsize(size_fmt)
+                    size = unpack(f"{endian()}{size_fmt}", serialized_data[offset:offset+ssize])[0]
+                    offset += ssize
+                    encoded_str = unpack(f"{endian()}{size}s", serialized_data[offset:offset+size])[0]
+                    decoded_str = encoded_str.decode(encoding)
+                    offset += size
+                    setattr(instance, field_name, decoded_str)
+                except struct_error:
+                    raise TypeError("Check endian in both functions.")
+            elif issubclass(field_type, NTString):
+                encoding = cls.string_encoding if hasattr(cls, "string_encoding") else "utf-8"
+                size = get_nt_string_length(serialized_data)
+                encoded_str = unpack(f"{endian()}{size}s", serialized_data[offset:offset+size])[0]
+                decoded_str = encoded_str.decode(encoding)
+                offset += size + 1
+                setattr(instance, field_name, decoded_str)
             else:
                 fmt = f"{endian()}{field_type.fmt()}"
                 size = calcsize(fmt)
